4c2f5b755595e3323d27559e6852fa8acf8f6ee7
JH <tnptt1122@gmail.com>
2023-08-21 01:57:50 +0900

    feat: sharedPreference 메서드 추가

--- app/src/main/java/com/watso/app/util/PreferenceUtil.kt
+++ app/src/main/java/com/watso/app/util/PreferenceUtil.kt
@@ -13,8 +13,44 @@ class PreferenceUtil(context: Context) {
         return prefs.getString(key, defValue).toString()
     }
 
+    fun getInt(key: String, defValue: Int): Int {
+        return prefs.getInt(key, defValue)
+    }
+
+    fun getLong(key: String, defValue: Long): Long {
+        return prefs.getLong(key, defValue)
+    }
+
+    fun getFloat(key: String, defValue: Float): Float {
+        return prefs.getFloat(key, defValue)
+    }
+
+    fun getBoolean(key: String, defValue: Boolean): Boolean {
+        return prefs.getBoolean(key, defValue)
+    }
+
     fun setString(key: String, str: String) {
         editor.putString(key, str).apply()
+        editor.commit()
+    }
+
+    fun setInt(key: String, value:Int) {
+        editor.putInt(key, value).apply()
+        editor.commit()
+    }
+
+    fun setLong(key: String, value: Long) {
+        editor.putLong(key, value).apply()
+        editor.commit()
+    }
+
+    fun setFloat(key: String, value:Float) {
+        editor.putFloat(key, value).apply()
+        editor.commit()
+    }
+
+    fun setBoolean(key: String, value:Boolean) {
+        editor.putBoolean(key, value).apply()
         editor.commit()
     }
 
--- app/src/main/java/com/watso/app/util/SessionManager.kt
+++ app/src/main/java/com/watso/app/util/SessionManager.kt
@@ -3,6 +3,7 @@ package com.watso.app.util
 import android.content.Context
 import android.content.SharedPreferences
 import com.watso.app.R
+import com.watso.app.feature.user.data.UserInfo
 
 object SessionManager {
 
@@ -10,11 +11,20 @@ object SessionManager {
     private const val REFRESH_TOKEN = "refreshToken"
 
     fun saveAccessToken(context: Context, token: String) {
-        setString(context, ACCESS_TOKEN, token)
+        saveString(context, ACCESS_TOKEN, token)
     }
 
     fun saveRefreshToken(context: Context, token: String) {
-        setString(context, REFRESH_TOKEN, token)
+        saveString(context, REFRESH_TOKEN, token)
+    }
+
+    fun saveUserInfo(context: Context, userInfo: UserInfo) {
+        saveLong(context, "userId", userInfo._id)
+        saveString(context, "name", userInfo.name)
+        saveString(context, "username", userInfo.username)
+        saveString(context, "nickname", userInfo.nickname)
+        saveString(context, "accountNumber", userInfo.accountNumber)
+        saveString(context, "email", userInfo.email)
     }
 
     fun getAccessToken(context: Context): String {
@@ -25,14 +35,36 @@ object SessionManager {
         return getString(context, REFRESH_TOKEN)
     }
 
-    fun setString(context: Context, key: String, value: String) {
+    fun getUserInfo(context: Context): UserInfo {
+        val _id = getLong(context, "userId")
+        val name = getString(context, "name")
+        val username = getString(context, "username")
+        val nickname = getString(context, "nickname")
+        val accountNumber = getString(context, "accountNumber")
+        val email = getString(context, "email")
+
+        return UserInfo(_id, name, username, nickname, accountNumber, email)
+    }
+
+    private fun saveString(context: Context, key: String, value: String) {
         val prefs = PreferenceUtil(context)
         prefs.setString(key, value)
     }
 
-    fun getString(context: Context, key: String): String {
+    private fun saveLong(context: Context, key: String, value: Long) {
+        val prefs = PreferenceUtil(context)
+        prefs.setLong(key, value)
+    }
+
+    private fun getString(context: Context, key: String): String {
         val prefs: SharedPreferences =
             context.getSharedPreferences(context.getString(R.string.app_name), Context.MODE_PRIVATE)
         return prefs.getString(key, "").toString()
+    }
+
+    private fun getLong(context: Context, key: String): Long {
+        val prefs: SharedPreferences =
+            context.getSharedPreferences(context.getString(R.string.app_name), Context.MODE_PRIVATE)
+        return prefs.getLong(key, -1)
     }
 }
